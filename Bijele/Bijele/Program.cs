0868d445816e5eeb1bbb397a079900be594b638f
Myszusz <Michal.Borkowski77@gmail.com>
2017-06-15 15:12:36 +0200

    Add files via upload

---
+++ Bijele/Bijele/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bijele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] fullSet = ("1 1 2 2 2 8").Split(' ');
+            String[] foundPieces = Console.ReadLine().Split(' ');
+            int[] missingPieces = new int[foundPieces.Length];
+            for (int i = 0; i < fullSet.Length; i++)
+            {
+                int fullSetNr = int.Parse(fullSet[i]);
+                int foundPiecesNr = int.Parse(foundPieces[i]);
+                missingPieces[i] = fullSetNr - foundPiecesNr;
+                Console.Write(missingPieces[i] + " ");
+            }
+        }
+    }
+}
---
+++ Bijele_stupid!!!/Bijele_stupid!!!/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bijele_stupid___
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] foundPieces = Console.ReadLine().Split(' ');
+            int nrOfKing = 1 - int.Parse(foundPieces[0]);
+            int nrOfQueen = 1 - int.Parse(foundPieces[1]);
+            int nrOfRook = 2 - int.Parse(foundPieces[2]);
+            int nrOfBishop = 2 - int.Parse(foundPieces[3]);
+            int nrOfKnight = 2 - int.Parse(foundPieces[4]);
+            int nrOfPawn = 8 - int.Parse(foundPieces[5]);
+            Console.WriteLine(nrOfKing + " " + nrOfQueen + " " + nrOfRook + " " + nrOfBishop + " " + nrOfKnight + " " + nrOfPawn);
+        }
+    }
+}
