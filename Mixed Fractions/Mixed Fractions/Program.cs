3108f72d08f3a6d0aba2ef87c93efb1a5ba25ca5
Myszusz <Michal.Borkowski77@gmail.com>
2017-06-15 15:15:24 +0200

    Add files via upload

---
+++ Mixed Fractions/Mixed Fractions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mixed_Fractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+            int numerator = int.Parse(array[0]);
+            int denominator = int.Parse(array[1]);
+            while (denominator != 0 && numerator != 0)
+            {
+                int restFromDv = numerator % denominator;
+                int numerator1 = numerator - restFromDv;
+                int wholeNr = numerator1 / denominator;
+                Console.WriteLine(wholeNr + " " + restFromDv + " " + "/" + " " + denominator);
+                string[] array1 = Console.ReadLine().Split(' ');
+                numerator = int.Parse(array1[0]);
+                denominator = int.Parse(array1[1]);
+            }
+        }
+    }
+}
---
+++ Oddities/Oddities/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oddities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nrOfLines = Console.ReadLine();
+            int nrOfLines1 = int.Parse(nrOfLines);
+            for (double i = 0; i < nrOfLines1; i++)
+            {
+                String num = Console.ReadLine();
+                int num1 = int.Parse(num);
+                int num2 = num1 % 2;
+                if (num2 == 0)
+                {
+                    Console.WriteLine(num1 + " is even");
+                }
+                else
+                {
+                    Console.WriteLine(num1 + " is odd");
+                }
+            }
+        }
+    }
+}
